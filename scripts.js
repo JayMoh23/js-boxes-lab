3a62d1e7c77a203ba1515b8acd5ccf98ad49eed1
jaymoh23 <jeffreyryanmurray@gmail.com>
2017-09-29 17:01:14 -0500

    deleting boxdivs

--- scripts.js
+++ scripts.js
@@ -8,8 +8,55 @@ document.addEventListener('DOMContentLoaded', function() {
          var boxDiv= document.createElement('div');
          boxDiv.className = 'boxes';
          document.body.appendChild(boxDiv);
+         boxDiv.setAttribute("id" , totalSquares.length);
+         totalSquares.push(boxDiv);
+         boxDiv.innerText= totalSquares.length;
+
+         boxDiv.addEventListener("mouseout", function(){
+             event.target.style.color="transparent"
+         })
+
+         boxDiv.addEventListener("mouseover", function(){
+             event.target.style.color="skyblue"
+         })
+             
+         
+
+         boxDiv.addEventListener('click', function(){
+         boxDiv.style.backgroundColor= getRandomColor();
+
+        
+         
+
+         })
+
+
+         boxDiv.addEventListener('dblclick', function(){
+         if (boxDiv.id %2){
+             alert("youre doing something right")
+         }
+         })
+        
+
+         
+
+
+         })
 
      })
 
-})
 
+
+
+
+var totalSquares = [];
+
+function getRandomColor(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i =0; i <6; i++) {
+        color += letters [Math. floor(Math.random() *16)];
+
+    }
+    return color;
+}
